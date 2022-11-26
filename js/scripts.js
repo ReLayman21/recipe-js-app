69c5d78e8d762bdb1459d3005820ae7df8046667
ReLayman21 <94310727+ReLayman21@users.noreply.github.com>
2022-11-26 10:35:48 -0500

    added array with cookie recipes and information

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert("Hello World");
-
-let favoriteFood = "Tacos";
-document.write(favoriteFood);
+let recipeList = [
+  { name: "Chocolate Chip Cookies", baked: true, dozensOfCookies: 6 },
+  { name: "Chocolate No Bake Cookies", baked: false, dozensOfCookies: 3 },
+  { name: "Coconut Macaroons", baked: true, dozensOfCookies: 2 },
+  { name: "Crunchies", baked: false, dozensOfCookies: 2 },
+  { name: "Haystacks", baked: false, dozensOfCookies: 3 },
+  { name: "Orange Cookies", baked: true, dozensOfCookies: 4 },
+];
