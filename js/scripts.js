398293e7e949f9a486be1659252d57f39737afec
ReLayman21 <94310727+ReLayman21@users.noreply.github.com>
2022-11-30 18:26:36 -0500

    css and js additions

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,87 @@ let recipeList = [
     category: "Dessert",
     baked: true,
     servings: 72,
+    ingredients: [
+      "shortening",
+      "sugar",
+      "brown sugar",
+      "eggs",
+      "flour",
+      "baking soda",
+      "salt",
+      "milk chocolate chips",
+    ],
   },
   {
     name: "Chocolate No Bake Cookies",
     category: "Dessert",
     baked: false,
     servings: 36,
+    ingredients: [
+      "sugar",
+      "milk",
+      "cocoa powder",
+      "butter",
+      "oatmeal",
+      "coconut",
+      "nuts",
+    ],
   },
-  { name: "Coconut Macaroons", category: "Dessert", baked: true, servings: 24 },
-  { name: "Crunchies", category: "Dessert", baked: false, servings: 24 },
-  { name: "Haystacks", category: "Dessert", baked: false, servings: 36 },
-  { name: "Orange Cookies", category: "Dessert", baked: true, servings: 48 },
+  {
+    name: "Coconut Macaroons",
+    category: "Dessert",
+    baked: true,
+    servings: 24,
+    ingredients: ["coconut", "sweetened condensed milk", "vanilla"],
+  },
+  {
+    name: "Crunchies",
+    category: "Dessert",
+    baked: false,
+    servings: 24,
+    ingredients: [
+      "chocolate almond bars",
+      "chow mein noodles",
+      "slivered almonds",
+    ],
+  },
+  {
+    name: "Haystacks",
+    category: "Dessert",
+    baked: false,
+    servings: 36,
+    ingredients: [
+      "butterscotch chips",
+      "peanut butter",
+      "mini marshmallows",
+      "chow mein noodles",
+    ],
+  },
+  {
+    name: "Orange Cookies",
+    category: "Dessert",
+    baked: true,
+    servings: 48,
+    ingredients: [
+      "shortening",
+      "brown sugar",
+      "eggs",
+      "orange rind",
+      "orange juice",
+      "flour",
+      "baking soda",
+      "salt",
+      "oatmeal",
+      "coconut",
+      "powdered sugar",
+    ],
+  },
 ];
+
+for (let i = 0; i < recipeList.length; i++) {
+  if (recipeList[i].baked && "Dessert") {
+    document.write(recipeList[i].name + " are baked in the oven");
+  } else {
+    document.write(recipeList[i].name + " do not need to be baked in the oven");
+  }
+}
