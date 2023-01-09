8730bfd85f9fcd0ebe62a0dc3c1ceac53f2019a8
ReLayman21 <94310727+ReLayman21@users.noreply.github.com>
2023-01-09 08:20:29 -0500

    updated html and js
    Added list item function inside forEach loop.
    Created an event listener to recipe when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,149 @@
 let recipeRepository = (function () {
-  let recipeList = [];
+  let recipeList = [
+    {
+      name: "Chocolate Chip Cookies",
+      category: "Dessert",
+      baked: true,
+      servings: 72,
+      ingredients: [
+        "shortening",
+        "sugar",
+        "brown sugar",
+        "eggs",
+        "flour",
+        "baking soda",
+        "salt",
+        "milk chocolate chips",
+      ],
+    },
+    {
+      name: "Chocolate No Bake Cookies",
+      category: "Dessert",
+      baked: false,
+      servings: 36,
+      ingredients: [
+        "sugar",
+        "milk",
+        "cocoa powder",
+        "butter",
+        "oatmeal",
+        "coconut",
+        "nuts",
+      ],
+    },
+    {
+      name: "Coconut Macaroons",
+      category: "Dessert",
+      baked: true,
+      servings: 24,
+      ingredients: ["coconut", "sweetened condensed milk", "vanilla"],
+    },
+    {
+      name: "Crunchies",
+      category: "Dessert",
+      baked: false,
+      servings: 24,
+      ingredients: [
+        "chocolate almond bars",
+        "chow mein noodles",
+        "slivered almonds",
+      ],
+    },
+    {
+      name: "Haystacks",
+      category: "Dessert",
+      baked: false,
+      servings: 36,
+      ingredients: [
+        "butterscotch chips",
+        "peanut butter",
+        "mini marshmallows",
+        "chow mein noodles",
+      ],
+    },
+    {
+      name: "Orange Cookies",
+      category: "Dessert",
+      baked: true,
+      servings: 48,
+      ingredients: [
+        "shortening",
+        "brown sugar",
+        "eggs",
+        "orange rind",
+        "orange juice",
+        "flour",
+        "baking soda",
+        "salt",
+        "oatmeal",
+        "coconut",
+        "powdered sugar",
+      ],
+    },
+  ];
+
+  function add(recipe) {
+    if (
+      typeof recipe === "object" &&
+      "name" in recipe &&
+      "category" in recipe &&
+      "baked" in recipe &&
+      "servings" in recipe &&
+      "ingredients" in recipe
+    ) {
+      recipeList.push(recipe);
+    } else {
+      console.log("recipe is not valid");
+    }
+  }
 
   function getAll() {
     return recipeList;
   }
-  function add(recipes) {
-    recipeList.push(recipes);
+
+  function addListItem(recipe) {
+    let repository = document.querySelector(".recipe-list");
+    let listRecipe = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = recipe.name;
+    button.classList.add("button-class");
+    button.addEventListener("click", (Event) => showDetails(recipe));
+    listRecipe.appendChild(button);
+    repository.appendChild(listRecipe);
   }
+
+  function showDetails(recipe) {
+    console.log(recipe);
+  }
+
   return {
-    getAll: getAll,
     add: add,
+    getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
 recipeRepository.add({
-  name: "Chocolate Chip Cookies",
+  name: "Sugar Cookies",
   category: "Dessert",
   baked: true,
-  servings: 72,
-  ingredients: [
-    "shortening",
-    "sugar",
-    "brown sugar",
-    "eggs",
-    "flour",
-    "baking soda",
-    "salt",
-    "milk chocolate chips",
-  ],
-});
-recipeRepository.add({
-  name: "Chocolate No Bake Cookies",
-  category: "Dessert",
-  baked: false,
   servings: 36,
   ingredients: [
-    "sugar",
-    "milk",
-    "cocoa powder",
     "butter",
-    "oatmeal",
-    "coconut",
-    "nuts",
-  ],
-});
-recipeRepository.add({
-  name: "Coconut Macaroons",
-  category: "Dessert",
-  baked: true,
-  servings: 24,
-  ingredients: ["coconut", "sweetened condensed milk", "vanilla"],
-});
-recipeRepository.add({
-  name: "Crunchies",
-  category: "Dessert",
-  baked: false,
-  servings: 24,
-  ingredients: [
-    "chocolate almond bars",
-    "chow mein noodles",
-    "slivered almonds",
-  ],
-});
-recipeRepository.add({
-  name: "Haystacks",
-  category: "Dessert",
-  baked: false,
-  servings: 36,
-  ingredients: [
-    "butterscotch chips",
-    "peanut butter",
-    "mini marshmallows",
-    "chow mein noodles",
-  ],
-});
-recipeRepository.add({
-  name: "Orange Cookies",
-  category: "Dessert",
-  baked: true,
-  servings: 48,
-  ingredients: [
     "shortening",
-    "brown sugar",
-    "eggs",
-    "orange rind",
-    "orange juice",
-    "flour",
-    "baking soda",
-    "salt",
-    "oatmeal",
-    "coconut",
     "powdered sugar",
+    "white sugar",
+    "eggs",
+    "flour",
+    "cream of tartar",
+    "baking soda",
+    "vanilla",
   ],
 });
 
-let recipeList = recipeRepository.getAll();
+recipeRepository.getAll().forEach(function (recipe) {
+  recipeRepository.addListItem(recipe);
+});
 
 // for (let i = 0; i < recipeList.length; i++) {
 //   if (recipeList[i].baked && "Dessert") {
@@ -112,16 +161,26 @@ let recipeList = recipeRepository.getAll();
 //   }
 // }
 
-recipeList.forEach(function (recipe) {
-  if (recipe.baked) {
-    document.write(recipe.name + " need to be baked in the oven.");
-  } else {
-    document.write(recipe.name + " do not need to be baked in the oven.");
-  }
-});
+// recipeList.forEach(function (recipe) {
+//   let recipe_list = document.querySelector(".recipe-list");
+//   let listRecipe = document.createElement("li");
+//   let button = document.createElement("button");
+//   button.innerText = recipe.name;
+//   button.classList.add("button-class");
+//   listRecipe.appendChild(button);
+//   recipe_list.appendChild(listRecipe);
+// });
 
-recipeList.forEach((recipe) => {
-  if (recipe.servings > 45) {
-    document.write(recipe.name + " makes a lot of cookies!");
-  }
-});
+// recipeList.forEach(function (recipe) {
+//   if (recipe.baked) {
+//     //document.write(recipe.name + " need to be baked in the oven.");
+//   } else {
+//    // document.write(recipe.name + " do not need to be baked in the oven.");
+//   }
+// });
+
+// recipeList.forEach((recipe) => {
+//   if (recipe.servings > 45) {
+//     document.write(recipe.name + " makes a lot of cookies!");
+//   }
+// });
